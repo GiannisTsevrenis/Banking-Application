8ce0a4e86ec34c4938c9f01eda37814920f57b0d
GiannisTsevrenis <ts3v@outlook.com>
2024-12-11 00:34:53 +0200

    Add client element UI

---
+++ src/main/java/org/bankingapp/Controllers/Admin/ClientCellController.java
@@ -0,0 +1,21 @@
+package org.bankingapp.Controllers.Admin;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientCellController implements Initializable {
+    public Label firstNameLabel;
+    public Label lastNameLabel;
+    public Label payeeAddressLabel;
+    public Label checkingAccountLabel;
+    public Label savingsAccountLabel;
+    public Label dateLabel;
+    public Button deleteButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+}
