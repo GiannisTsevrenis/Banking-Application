43f2c2685fc80cb6d2ab314e4e95b3c7cc0267ab
GiannisTsevrenis <ts3v@outlook.com>
2024-12-12 02:46:55 +0200

    Add client creation from admin panel

--- src/main/java/org/bankingapp/Controllers/Admin/CreateClientController.java
+++ src/main/java/org/bankingapp/Controllers/Admin/CreateClientController.java
@@ -4,8 +4,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import org.bankingapp.Models.Model;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 public class CreateClientController implements Initializable {
@@ -20,8 +23,76 @@ public class CreateClientController implements Initializable {
     public TextField savingsAccountBalanceField;
     public Button createClientButton;
     public Label errorLabel;
+    private String payeeAddress;
+    private boolean createCheckingAccountFlag = false;
+    private boolean createSavingsAccountFlag = false;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        createClientButton.setOnAction(event -> createClient());
+        payeeAddressCheckBox.selectedProperty().addListener((observableValue, oldValue, newValue) -> {
+            if (newValue) {
+                payeeAddress = createPayeeAddress();
+                onCreatePayeeAddress();
+            }
+        });
+        checkingAccountCheckBox.selectedProperty().addListener((observableValue, oldValue, newValue) -> {
+            if ((newValue)) {
+                createCheckingAccountFlag = true;
+            }
+        });
+        savingsAccountCheckBox.selectedProperty().addListener((observableValue, oldValue, newValue) -> {
+            if ((newValue)) {
+                createSavingsAccountFlag = true;
+            }
+        });
+    }
+
+    private void createClient() {
+        if (createCheckingAccountFlag) {
+            createAccount("Checking");
+        }
+        if (createSavingsAccountFlag) {
+            createAccount("Savings");
+        }
+        Model.getInstance().getDatabaseDriver().createClient(firstNameField.getText(), lastNameField.getText(), payeeAddress, passwordField.getText(), LocalDate.now());
+        errorLabel.setText("Client created successfully.");
+        emptyFields();
+    }
+
+    private void createAccount(String accountType) {
+        double balance = Double.parseDouble(checkingAccountBalanceField.getText());
+        String firstSection = "4321";
+        String lastSection = Integer.toString((new Random()).nextInt(9000) + 1000);
+        String accountNumber = firstSection + " " + lastSection;
+        if (accountType.equals("Checking")) {
+            Model.getInstance().getDatabaseDriver().createCheckingAccount(payeeAddress, accountNumber, 2500, balance);
+        } else {
+            Model.getInstance().getDatabaseDriver().createSavingsAccount(payeeAddress, accountNumber, 500, balance);
+        }
+    }
+
+    private void onCreatePayeeAddress() {
+        if (firstNameField.getText() != null & lastNameField.getText() != null) {
+            payeeAddressLabel.setText(payeeAddress);
+        }
+    }
+
+    private String createPayeeAddress() {
+        int currentIncrement = Model.getInstance().getDatabaseDriver().getLastId() + 1;
+        char firstCharacter = Character.toLowerCase(firstNameField.getText().charAt(0));
+        return "@" + firstCharacter + lastNameField.getText() + currentIncrement;
+    }
+
+    private void emptyFields() {
+        firstNameField.setText("");
+        lastNameField.setText("");
+        passwordField.setText("");
+        payeeAddressCheckBox.setSelected(false);
+        payeeAddressLabel.setText("");
+        checkingAccountCheckBox.setSelected(false);
+        checkingAccountBalanceField.setText("");
+        savingsAccountCheckBox.setSelected(false);
+        savingsAccountBalanceField.setText("");
     }
 }
--- src/main/java/org/bankingapp/Models/DatabaseDriver.java
+++ src/main/java/org/bankingapp/Models/DatabaseDriver.java
@@ -1,5 +1,6 @@
 package org.bankingapp.Models;
 import java.sql.*;
+import java.time.LocalDate;
 
 public class DatabaseDriver {
     private Connection connection;
@@ -27,15 +28,84 @@ public class DatabaseDriver {
 
     //admin
     public ResultSet getAdminData(String username, String password) {
-        Statement statement;
+        PreparedStatement preparedStatement;
         ResultSet resultSet;
+        String query = "SELECT * FROM Admins WHERE Username = ? AND Password = ?";
         try {
-            statement = this.connection.createStatement();
-            resultSet = statement.executeQuery("SELECT * FROM Admins WHERE Username='" + username + "' AND Password='" + password + "';");
+            preparedStatement = this.connection.prepareStatement(query);
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+            resultSet = preparedStatement.executeQuery();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         return resultSet;
     }
+
+    public void createClient(String firstName, String lastName, String address, String password, LocalDate date) {
+        PreparedStatement preparedStatement;
+        String query = "INSERT INTO Clients (FirstName, LastName, PayeeAddress, Password, Date) VALUES (?, ?, ?, ?, ?)";
+        try {
+            preparedStatement = this.connection.prepareStatement(query);
+            preparedStatement.setString(1, firstName);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setString(3, address);
+            preparedStatement.setString(4, password);
+            preparedStatement.setString(5, String.valueOf(date));
+            preparedStatement.executeUpdate();
+//            int rowsInserted = preparedStatement.executeUpdate();
+//            if (rowsInserted > 0) {
+//                System.out.println("success");
+//
+//            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void createCheckingAccount(String owner, String number, double transactionLimit, double balance) {
+        PreparedStatement preparedStatement;
+        String query = "INSERT INTO CheckingAccounts (Owner, AccountNumber, TransactionLimit, Balance) VALUES (?, ?, ?, ?)";
+        try {
+            preparedStatement = this.connection.prepareStatement(query);
+            preparedStatement.setString(1, owner);
+            preparedStatement.setString(2, number);
+            preparedStatement.setString(3, String.valueOf(transactionLimit));
+            preparedStatement.setString(4, String.valueOf(balance));
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void createSavingsAccount(String owner, String number, double withdrawalLimit, double balance) {
+        PreparedStatement preparedStatement;
+        String query = "INSERT INTO SavingsAccounts (Owner, AccountNumber, WithdrawalLimit, Balance) VALUES (?, ?, ?, ?)";
+        try {
+            preparedStatement = this.connection.prepareStatement(query);
+            preparedStatement.setString(1, owner);
+            preparedStatement.setString(2, number);
+            preparedStatement.setString(3, String.valueOf(withdrawalLimit));
+            preparedStatement.setString(4, String.valueOf(balance));
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     //common methods
+    public int getLastId(){
+        PreparedStatement preparedStatement;
+        ResultSet resultSet;
+        String query = "SELECT * FROM sqlite_sequence WHERE name = 'Clients'";
+        int id = 0;
+        try {
+            preparedStatement = this.connection.prepareStatement(query);
+            resultSet = preparedStatement.executeQuery();
+            // if resultSet.next() ?
+            id = resultSet.getInt("seq");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return id;
+    }
 }
